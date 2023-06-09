951a27130608a0def673381dcf530a835c8c2ac8
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-09 01:28:44 -0300

    - Home page

--- subMe-RectAngular/subMe/src/app/app.module.ts
+++ subMe-RectAngular/subMe/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
